76b3ea2d3a3c75339b5c599aed224c2ebd8671a7
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-07-07 12:26:49 +0200

    Camera App - Init

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+const constraints = { video: { facingMode: "environment" }, audio: false };
+let track = null;
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+
+function cameraStart() {
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+cameraTrigger.addEventListener("click", function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+});
+
+window.addEventListener("load", cameraStart, false);
